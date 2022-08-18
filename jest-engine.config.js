115ce098b032868ceec775a308eb379664723b76
Yu-Chia (Gerry) Wu <gges5110@gmail.com>
2022-08-18 00:29:57 -0500

    9 integrate with codecov (#12)
    * fix: generate report in main branch

--- jest-engine.config.js
+++ jest-engine.config.js
@@ -2,4 +2,5 @@ module.exports = {
   collectCoverageFrom: ["<rootDir>/src/engine/**/*.tsx"],
   testMatch: ["<rootDir>/src/engine/**/*.test.tsx"],
   testPathIgnorePatterns: ["<rootDir>/src/components/"],
+  coverageReporters: ["text", "cobertura"],
 };
