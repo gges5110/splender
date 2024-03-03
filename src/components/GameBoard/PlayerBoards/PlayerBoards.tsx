67a9ec1ad7fe147d567713245caf7ee744d59202
Gerry Wu <ycwu@crunchbase.com>
2024-03-02 22:51:12 -0600

    show gems sum

--- src/components/GameBoard/PlayerBoards/PlayerBoards.tsx
+++ src/components/GameBoard/PlayerBoards/PlayerBoards.tsx
@@ -33,62 +33,68 @@ export const PlayerBoards: FC<PlayerBoardsProps> = ({
         }
         sx={{ overflowY: "hidden" }}
       >
-        {players.map((player, index: number) => (
-          <div
-            className={clsx(
-              "flex items-center bg-slate-200 my-4 rounded-xl relative p-2 pt-8 shadow-md",
-              { "bg-slate-400": Number(currentPlayer) === index }
-            )}
-            key={index}
-          >
-            <Button
-              className={clsx(
-                "absolute leading-8 -top-5 -left-4 w-fit h-8 rounded-lg bg-blue-300 px-3"
-              )}
-              onClick={() => setPlayerDialogIndex(index)}
-            >
-              <Badge
-                color={"success"}
-                invisible={!match.players[index]?.isConnected}
-                variant={"dot"}
-              >
-                <span className={"text-slate-700"}>
-                  {match?.players[index].name ||
-                    `Bot ${match?.players[index].id}`}
-                  {match?.players[index].id === Number(playerID) && <> (you)</>}
-                </span>
-              </Badge>
-            </Button>
-            <PlayerDialog
-              closePlayerDialog={() => {
-                setPlayerDialogIndex(undefined);
-              }}
-              player={players[index]}
-              playerDialogOpen={playerDialogIndex === index}
-            />
-            <div className={"w-2"} />
-            <div className={"text-center"}>
-              <PlayerGems gems={player.gems} />
-              <div className={"h-2"} />
-              <PlayerCards
-                buildFromReserve={buildFromReserve}
-                cards={player.cards}
-                isActivePlayer={Number(currentPlayer) === index}
-                onCardClick={() => setPlayerDialogIndex(index)}
-                player={player}
-                reservedCards={player.reservedCards}
-              />
-            </div>
-            <div className={"w-8"} />
+        {players.map((player, index: number) => {
+          const playerName =
+            match?.players[index].name || `Bot ${match?.players[index].id}`;
+          return (
             <div
-              className={
-                "absolute leading-8 top-1 right-2 text-center w-18 h-8 rounded-xl font-semibold text-yellow-500"
-              }
+              className={clsx(
+                "flex items-center bg-slate-200 my-4 rounded-xl relative p-2 pt-8 shadow-md",
+                { "bg-slate-400": Number(currentPlayer) === index }
+              )}
+              key={index}
             >
-              {getPlayerPoints(player)} points
+              <Button
+                className={clsx(
+                  "absolute leading-8 -top-5 -left-4 w-fit h-8 rounded-lg bg-blue-300 px-3"
+                )}
+                onClick={() => setPlayerDialogIndex(index)}
+              >
+                <Badge
+                  color={"success"}
+                  invisible={!match.players[index]?.isConnected}
+                  variant={"dot"}
+                >
+                  <span className={"text-slate-700"}>
+                    {playerName}
+                    {match?.players[index].id === Number(playerID) && (
+                      <> (you)</>
+                    )}
+                  </span>
+                </Badge>
+              </Button>
+              <PlayerDialog
+                closePlayerDialog={() => {
+                  setPlayerDialogIndex(undefined);
+                }}
+                player={players[index]}
+                playerDialogOpen={playerDialogIndex === index}
+                playerName={playerName}
+              />
+              <div className={"w-2"} />
+              <div className={"text-center"}>
+                <PlayerGems gems={player.gems} />
+                <div className={"h-2"} />
+                <PlayerCards
+                  buildFromReserve={buildFromReserve}
+                  cards={player.cards}
+                  isActivePlayer={Number(currentPlayer) === index}
+                  onCardClick={() => setPlayerDialogIndex(index)}
+                  player={player}
+                  reservedCards={player.reservedCards}
+                />
+              </div>
+              <div className={"w-8"} />
+              <div
+                className={
+                  "absolute leading-8 top-1 right-2 text-center w-18 h-8 rounded-xl font-semibold text-yellow-500"
+                }
+              >
+                {getPlayerPoints(player)} points
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </Box>
     </Paper>
   );
--- src/components/GameBoard/PlayerBoards/PlayerGems/PlayerGems.tsx
+++ src/components/GameBoard/PlayerBoards/PlayerGems/PlayerGems.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
 import { GemDisplay } from "../../../Shared/GemDisplay/GemDisplay";
+import { Box } from "@mui/material";
+import DiamondOutlinedIcon from "@mui/icons-material/DiamondOutlined";
 
 interface PlayerGemsProps {
   gems: number[];
 }
 
 export const PlayerGems: React.FC<PlayerGemsProps> = ({ gems }) => (
-  <div className={"flex gap-1 sm:gap-4"}>
+  <div className={"flex gap-1 sm:gap-4 items-center"}>
     {gems.map((gemCount, index) => (
       <div className={"gem-size"} key={index}>
         {gemCount > 0 && <GemDisplay color={index} count={gemCount} />}
       </div>
     ))}
+    <Box>
+      {gems.reduce((p, v) => p + v, 0)}
+      <DiamondOutlinedIcon />
+    </Box>
   </div>
 );
