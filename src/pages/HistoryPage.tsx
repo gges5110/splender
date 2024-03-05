80b71343462e322221ca98b214e5287c7a395bf3
Yu-Chia (Gerry) Wu <gges5110@gmail.com>
2024-03-05 11:40:41 -0600

    redirect to app even on 404 (#51)

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,7 +1,15 @@
 import { FC } from "react";
 import { useAtomValue } from "jotai";
 import { historyAtom } from "src/Atoms";
-import { Paper } from "@mui/material";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 export interface GameHistory {
   date: string;
@@ -14,11 +22,49 @@ export interface GameHistory {
 
 interface HistoryProps {}
 export const HistoryPage: FC<HistoryProps> = () => {
-  const history = useAtomValue(historyAtom);
   return (
     <Paper>
-      HistoryPage
-      {JSON.stringify(history)}
+      <HistoryTable />
     </Paper>
+  );
+};
+
+const HistoryTable = () => {
+  const history = useAtomValue(historyAtom);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label={"simple table"} sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell align={"right"}>Number of Players</TableCell>
+            <TableCell align={"right"}>Date</TableCell>
+            <TableCell align={"right"}>Turns</TableCell>
+            <TableCell align={"right"}>Winner</TableCell>
+            <TableCell align={"right"}>Seed</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {history.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component={"th"} scope={"row"}>
+                {row.id}
+              </TableCell>
+              <TableCell align={"right"}>{row.numberOfPlayers}</TableCell>
+              <TableCell align={"right"}>
+                {new Date(row.date).toLocaleString()}
+              </TableCell>
+              <TableCell align={"right"}>{row.turns}</TableCell>
+              <TableCell align={"right"}>{row.winner}</TableCell>
+              <TableCell align={"right"}>{row.seed}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import svgrPlugin from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
+import { join, parse, resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "build",
+    rollupOptions: {
+      input: entryPoints("index.html", "404.html"),
+    },
   },
   plugins: [svgrPlugin(), react()],
   base: "/splender/",
@@ -16,3 +20,15 @@ export default defineConfig({
     },
   },
 });
+
+function entryPoints(...paths: any[]) {
+  const entries = paths.map(parse).map((entry) => {
+    const { dir, base, name, ext } = entry;
+    const key = join(dir, name);
+    const path = resolve(__dirname, dir, base);
+    return [key, path];
+  });
+
+  const config = Object.fromEntries(entries);
+  return config;
+}
