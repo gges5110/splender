5a65b16a520d67cf9bb5b97e726034991c8a5c7d
Gerry Wu <gges5110@gmail.com>
2025-02-15 23:44:42 -0600

    chore: material ui 6

--- src/components/GameBoard/PlayingTable/GemsSection/GemsPicker/SelectedGems/SelectedGem.tsx
+++ src/components/GameBoard/PlayingTable/GemsSection/GemsPicker/SelectedGems/SelectedGem.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "motion/react";
 import { Button } from "@mui/material";
 import { colorIndexToPalette } from "src/styles/paletteTheme";
 
